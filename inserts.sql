fc15bdbf6adf2ebc7964b7495d0b51d240f01e71
luisreyna93 <luisreyna93@gmail.com>
2015-04-27 23:09:00 -0500

    inserts

---
+++ inserts.sql
@@ -0,0 +1,74 @@
+
+INSERT INTO `jeopardy`.`gameprofile` (`id`, `name`, `userId`) VALUES (1, 'prueba1', 1);
+INSERT INTO `jeopardy`.`gameprofile` (`id`, `name`, `userId`) VALUES (2, 'prueba2', 1);
+INSERT INTO `jeopardy`.`gameprofile` (`id`, `name`, `userId`) VALUES (3, 'prueba3', 1);
+
+
+INSERT INTO `jeopardy`.`class` (`id`, `name`) VALUES (1, 'classprueba');
+INSERT INTO `jeopardy`.`category` (`id`, `name`) VALUES (1, 'categoryprueba');
+
+
+INSERT INTO `jeopardy`.`categorygameprofile` (`categoryId`, `gameProfileId`) VALUES (1, 1);
+INSERT INTO `jeopardy`.`categorygameprofile` (`categoryId`, `gameProfileId`) VALUES (1, 1);
+INSERT INTO `jeopardy`.`categorygameprofile` (`categoryId`, `gameProfileId`) VALUES (1, 1);
+INSERT INTO `jeopardy`.`categorygameprofile` (`categoryId`, `gameProfileId`) VALUES (1, 1);
+INSERT INTO `jeopardy`.`categorygameprofile` (`categoryId`, `gameProfileId`) VALUES (1, 1);
+
+
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (1, 'pregunta1', 'respuesta1', 1, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (2, 'pregunta2', 'respuesta1', 2, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (3, 'pregunta3', 'respuesta1', 3, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (4, 'pregunta4', 'respuesta1', 4, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (5, 'pregunta5', 'respuesta1', 5, 1);
+
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (6, 'pregunta1', 'respuesta1', 1, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (7, 'pregunta2', 'respuesta1', 2, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (8, 'pregunta3', 'respuesta1', 3, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (9, 'pregunta4', 'respuesta1', 4, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (10, 'pregunta5', 'respuesta1', 5, 1);
+
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (11, 'pregunta1', 'respuesta1', 1, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (12, 'pregunta2', 'respuesta1', 2, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (13, 'pregunta3', 'respuesta1', 3, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (14, 'pregunta4', 'respuesta1', 4, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (15, 'pregunta5', 'respuesta1', 5, 1);
+
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (16, 'pregunta1', 'respuesta1', 1, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (17, 'pregunta2', 'respuesta1', 2, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (18, 'pregunta3', 'respuesta1', 3, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (19, 'pregunta4', 'respuesta1', 4, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (20, 'pregunta5', 'respuesta1', 5, 1);
+
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (21, 'pregunta1', 'respuesta1', 1, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (22, 'pregunta2', 'respuesta1', 2, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (23, 'pregunta3', 'respuesta1', 3, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (24, 'pregunta4', 'respuesta1', 4, 1);
+INSERT INTO `jeopardy`.`question` (`id`, `question`, `answer`, `difficulty`, `categoryId`) VALUES (25, 'pregunta5', 'respuesta1', 5, 1);
+
+
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (1, 1);
+
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (2, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (3, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (4, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (5, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (6, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (7, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (8, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (9, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (10, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (11, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (12, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (13, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (14, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (15, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (16, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (17, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (18, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (19, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (20, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (21, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (22, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (23, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (24, 1);
+INSERT INTO `jeopardy`.`questiongameprofile` (`questionId`, `gameProfileId`) VALUES (25, 1);
